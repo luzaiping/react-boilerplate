ff7c39e067af09125c4ba079191217244a467de8
zaiping.lu <zaiping.lu@luckincoffee.com>
2019-07-16 16:15:12 +0800

    去掉没用的注释代码

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
-// import { routerReducer } from 'react-router-redux';
 import counter from './counter';
 import users from './article';
-// import { i18nConfig } from 'config'
 
 const root = {
   counter,
