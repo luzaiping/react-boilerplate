2eafe3a7898a4d422f71ff94541a2e1b9ff68857
zaiping.lu <zaiping.lu@luckincoffee.com>
2020-03-02 22:22:00 +0800

    feat: add enzyme testing

--- __test__/App.spec.js
+++ __test__/App.spec.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+import axios from 'axios';
 
 import App, { dataReducer } from '../src/App';
+import Counter from '../src/Counter';
 
 const list = ['a', 'b', 'c'];
 
@@ -38,6 +41,91 @@ describe('App', () => {
       });
       expect(newState).toEqual({ list: [], error: true });
       expect(newState.error).toBeTruthy();
+    });
+
+    it('passes all props to Counter', () => {
+      const appWrapper = mount(<App />);
+      const counterWrapper = appWrapper.find(Counter);
+
+      expect(counterWrapper.find('p').text()).toEqual('0');
+    });
+
+    it('increments the counter', () => {
+      const wrapper = mount(<App />);
+
+      wrapper
+        .find('button')
+        .at(0)
+        .simulate('click');
+
+      const counterWrapper = wrapper.find(Counter);
+      expect(counterWrapper.find('p').text()).toBe('1');
+    });
+
+    it('decrements the counter', () => {
+      const wrapper = mount(<App />);
+
+      wrapper
+        .find('button')
+        .at(1)
+        .simulate('click');
+
+      const counterWrapper = wrapper.find(Counter);
+      expect(counterWrapper.find('p').text()).toBe('-1');
+    });
+
+    it('fetches async data', () => {
+      const promise = new Promise(resolve => {
+        setTimeout(() => {
+          resolve({
+            data: {
+              hits: [
+                { objectID: '1', title: 'a' },
+                { objectID: '2', title: 'b' }
+              ]
+            }
+          });
+        }, 100);
+      });
+
+      axios.get = jest.fn(() => promise);
+
+      const wrapper = mount(<App />);
+      expect(wrapper.find('li').length).toEqual(0);
+
+      promise.then(() => {
+        setImmediate(() => {
+          wrapper.update();
+          expect(wrapper.find('li').length).toEqual(2);
+          axios.get.mockClear();
+
+          // done();
+        });
+      });
+    });
+
+    it('fetches async data but fails', () => {
+      const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          reject(new Error('Whoops!'));
+        }, 100);
+      });
+
+      axios.get = jest.fn(() => promise);
+
+      const wrapper = mount(<App />);
+
+      promise.catch(() => {
+        setImmediate(() => {
+          wrapper.update();
+
+          expect(wrapper.find('li').length).toBe(0);
+          expect(wrapper.find('.error').length).toBe(1);
+
+          axios.get.mockClear();
+          // done();
+        });
+      });
     });
   });
 });
