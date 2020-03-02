adf61f2628c14f3f558abc843f053399275a143f
zaiping.lu <zaiping.lu@luckincoffee.com>
2020-03-02 17:51:05 +0800

    feat: 集成 enzyme
    - 去掉  package-lock.json，改用 yarn
    - 增加 counter 的 enzyme test case

--- __test__/Counter.spec.js
+++ __test__/Counter.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
 
 import Counter from '../src/Counter';
 
@@ -8,5 +9,10 @@ describe('Counter', () => {
     const component = renderer.create(<Counter count={0} />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('renders the Counter', () => {
+    const wrapper = mount(<Counter />);
+    expect(wrapper.find(Counter).length).toBe(1);
   });
 });
